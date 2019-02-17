8aa5ffba30adbe6d4557de76ef869fdb7c571f78
AMutaa <piusandru@gmail.com>
2019-02-17 18:21:56 -0500

    first mutation component

--- src/Posts/NewPost.js
+++ src/Posts/NewPost.js
@@ -19,12 +19,42 @@ export default class NewPost extends Component {
       <div>
         <h1>New Post</h1>
         {/* <PostForm /> */}
-        <form>
-          <input name="title" type="text" onChange={this.handleInput} value={title} placeholder="title" />
-          <textarea name="body" type="text" onChange={this.handleInput} value={body} placeholder="body" id="" cols="30" rows="10" />
-          <button>Submit</button>
-        </form>
+        <Mutation mutation={NEW_POST} variables={{ title, body }}>
+          {createPost => (
+            <form onSubmit={(e) => {
+              e.preventDefault();
+              createPost().then(
+                () => {
+                  this.setState({
+                    title: '',
+                    body: ''
+                  })
+                }
+              ).catch(e => console.log(e))
+            }}>
+              <input name="title" type="text" onChange={this.handleInput} value={title} placeholder="title" />
+              <textarea name="body" type="text" onChange={this.handleInput} value={body} placeholder="body" id="" cols="30" rows="10" />
+              <button>Submit</button>
+            </form>
+          )}
+
+        </Mutation>
       </div>
     )
   }
 }
+
+
+const NEW_POST = gql`
+ mutation createPost($title: String!, $body: String! ) {
+   createPost(data: {
+    status: PUBLISHED
+     title: $title
+     body:  $body
+   }) {
+     title
+     body
+     id
+   }
+ }
+`
