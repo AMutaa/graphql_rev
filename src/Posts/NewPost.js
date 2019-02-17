a8a0d0bf9dcc7bb41c5f8d9fa7e9a50f675304ee
AMutaa <piusandru@gmail.com>
2019-02-17 18:01:08 -0500

    setting up form component"

--- src/Posts/NewPost.js
+++ src/Posts/NewPost.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
+import PostForm from './PostForm'
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
 
 export default class NewPost extends Component {
+  state = {
+    title: '',
+    body: ''
+  }
+  handleInput = (e) => {
+    const formData = {}
+    formData[e.target.name] = e.target.value
+    this.setState({ ...formData })
+  }
   render() {
+    const { title, body } = this.state
     return (
       <div>
-
+        <h1>New Post</h1>
+        {/* <PostForm /> */}
+        <form>
+          <input name="title" type="text" onChange={this.handleInput} value={title} placeholder="title" />
+          <textarea name="body" type="text" onChange={this.handleInput} value={body} placeholder="body" id="" cols="30" rows="10" />
+          <button>Submit</button>
+        </form>
       </div>
     )
   }
--- src/Posts/PostForm.js
+++ src/Posts/PostForm.js
@@ -4,7 +4,9 @@ export default class PostForm extends Component {
   render() {
     return (
       <form>
-
+        <input type="text" placeholder="title" />
+        <textarea type="text" placeholder="body" id="" cols="30" rows="10" />
+        <button>Submit</button>
       </form>
     )
   }
