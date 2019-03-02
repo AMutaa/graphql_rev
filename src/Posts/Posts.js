6264a0d5505da1ccb5e7b894c37f13f155212c9a
AMutaa <piusandru@gmail.com>
2019-03-02 15:27:30 -0500

    updated query with sorting and pagination

--- src/Posts/Posts.js
+++ src/Posts/Posts.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import { Link } from 'react-router-dom';
@@ -10,22 +10,28 @@ export default class Posts extends Component {
     return (
       <div>
         <Link className='button' to={'/post/new'}>New Post</Link>
-        <ul className='posts'>
+        <ol className='posts'>
           <Query query={POSTS_QUERY}>
-            {({ loading, data }) => {
+            {({ loading, data, fetchMore }) => {
               if (loading) return 'Loading...';
               const { posts } = data;
-              return posts.map(post => (
-                <li key={post.id} >
-                  <Link to={`/post/${post.id}`}>
-                    <h1>{post.title}</h1>
-                  </Link>
-                </li>
-              ));
+              return (
+                <Fragment>
+                  {posts.map(post => (
+                    <li key={post.id} >
+                      <Link to={`/post/${post.id}`}>
+                        <h1>{post.title}</h1>
+                      </Link>
+                    </li>
+                  ))}
+                  <li><button className='button' onClick={fetchMore}>Load More</button></li>
+                </Fragment>
+              )
+
             }}
 
           </Query>
-        </ul>
+        </ol>
       </div>
 
     )
@@ -34,12 +40,13 @@ export default class Posts extends Component {
 
 
 // writing the first query
+// adjusted query with sorting and pagination
 const POSTS_QUERY = gql`
  query allPosts{
-   posts {
+  posts(orderBy: createdAt_DESC, first: 3)  {
     id
     title
     body
-   }
  }
+}
  `
